acb07eac218f9d8ce62a501adfbd80c721748de5
Alistair Buxton <a.j.buxton@gmail.com>
2012-06-22 00:33:47 +0100

    Add basics of Xinerama support.
    Now inside XGetGeometry we query the Xinerama setup of the display.
    This doesn't do anything but write it out to debug. The idea is
    to pick the "fake" size from this list.

--- fullscreenhack.c
+++ fullscreenhack.c
@@ -9,11 +9,16 @@
 #include <dlfcn.h>
 #include <stdio.h>
 #include <X11/Xlib.h>
+#include <X11/extensions/Xinerama.h>
 
 typedef Status (*xgg_func)(Display *, Drawable, Window *, 
 				int *, int *, unsigned int *, 
 				unsigned int *, unsigned int *, 
 				unsigned int *);
+
+typedef XineramaScreenInfo * (*xsi_func)(Display*,int*);
+
+typedef int (*xfree_func) (void *);
 
 void __attribute__ ((constructor)) load(void);
 
@@ -40,8 +45,21 @@ Status XGetGeometry(Display *display, Drawable d, Window *root_return,
                 border_width_return, depth_return);
 
     if (d == RootWindow(display, DefaultScreen(display))) {
+        xfree_func XFree = dlsym(xlib_handle, "XFree");
+        void *xin_handle;
+        XineramaScreenInfo *screens;
+        int n_screens, n;
+        xin_handle = dlopen("libXinerama.so", RTLD_LAZY);
+        xsi_func qs = dlsym(xin_handle, "XineramaQueryScreens");
+        screens = qs(display, &n_screens);
+
+        for(n=0;n<n_screens;n++)
+            fprintf(stderr, "screen %d, %dx%d+%d+%d\n", n, screens[n].width, screens[n].height, screens[n].x_org, screens[n].y_org);
+
         *width_return = FAKE_WIDTH;
         *height_return = FAKE_HEIGHT;
+
+        XFree(screens);
     }
     return s;
 
