f533cdfd07b0c3b8f1cd90c0a768aa10db7955bd
Alistair Buxton <a.j.buxton@gmail.com>
2012-06-21 23:31:36 +0100

    Fix the whitespace.

--- fullscreenhack.c
+++ fullscreenhack.c
@@ -21,33 +21,32 @@ void __attribute__ ((constructor)) load(void);
 // Called when the library is loaded and before dlopen() returns
 void load(void)
 {
-	fprintf(stderr, "fullscreen hack loaded...\n");
+    fprintf(stderr, "fullscreen hack loaded...\n");
 }
 
 Status XGetGeometry(Display *display, Drawable d, Window *root_return, 
-			int *x_return, int *y_return, 
-			unsigned int *width_return, 
-			unsigned int *height_return, 
-			unsigned int *border_width_return, 
-			unsigned int *depth_return)
+            int *x_return, int *y_return, 
+            unsigned int *width_return, 
+            unsigned int *height_return, 
+            unsigned int *border_width_return, 
+            unsigned int *depth_return)
 {
+    typedef Status (*func)(Display *, Drawable, Window *, 
+                int *, int *, unsigned int *, 
+                unsigned int *, unsigned int *, 
+                unsigned int *);
+    void *handle;
+    Status s;
+    handle = dlopen("libX11.so", RTLD_LAZY);
+    func orig = dlsym(handle, "XGetGeometry");
+    s = orig(display, d, root_return, 
+                x_return, y_return, 
+                width_return, height_return, 
+                border_width_return, depth_return);
 
-	typedef Status (*func)(Display *, Drawable, Window *, 
-				int *, int *, unsigned int *, 
-				unsigned int *, unsigned int *, 
-				unsigned int *);
-	void *handle;
-	Status s;
-	handle = dlopen("libX11.so", RTLD_LAZY);
-	func orig = dlsym(handle, "XGetGeometry");
-	s = orig(display, d, root_return, 
-		x_return, y_return, 
-		width_return, height_return, 
-		border_width_return, depth_return);
-
-	if(*width_return == REAL_WIDTH) *width_return = FAKE_WIDTH;
-	if(*height_return == REAL_HEIGHT) *height_return = FAKE_HEIGHT;
-	return s;
+    if(*width_return == REAL_WIDTH) *width_return = FAKE_WIDTH;
+    if(*height_return == REAL_HEIGHT) *height_return = FAKE_HEIGHT;
+    return s;
 
 }
 
